7b6aebb6674e2871308336c3b699ddc68f40910e
Paul Uzzal <uzzalpaul70@hotmail.com>
2020-05-16 17:48:24 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
-var name = "Paul";
-var height = 70;
+var name = 'Paul';
+var height = '70';
 
-console.log("Name:");
+console.log('Name:');
 console.log(name);
 
-console.log("Height:");
+console.log('Height:');
 console.log(height+1);
